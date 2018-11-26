d4efe4f49c7ced40b88ff132dcbfbdfbd8035609
w232vfgf <1057578625@qq.com>
2018-11-26 16:40:07 +0800

    添加项目

---
+++ 项目/京东-购物页/js/main.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        model: 'grid',
+        products: [{
+            title: 'Apple iPhone X',
+            thumb: 'https://img13.360buyimg.com/mobilecms/s270x270_jfs/t10675/253/1344769770/66891/92d54ca4/59df2e7fN86c99a27.jpg!q70.dpg.webp',
+            price: 6999.00,
+            isDouble11: true
+        }, {
+            title: '荣耀9i 4GB+64GB 幻夜黑 移动联通电信4G全面屏手机 双卡双待',
+            thumb: 'https://img12.360buyimg.com/mobilecms/s270x270_jfs/t21415/332/642302956/189613/778f2021/5b13cd6cN8e12d4aa.jpg!q70.dpg.webp',
+            price: 1299.00,
+            isDouble11: true
+        }, {
+            title: '黑莓（BlackBerry）KEY2标准版 6GB+64GB双卡双待 4G全网通手机 黑色 移动联通电信手机',
+            thumb: 'https://img10.360buyimg.com/mobilecms/s270x270_jfs/t25855/259/1224767986/141142/af393ca6/5b8e328dN0671ae1e.jpg!q70.dpg.webp',
+            price: 3699.00,
+            isDouble11: false
+        }, {
+            title: 'Apple iPhone 8 64GB 红色特别版 移动联通电信4G手机',
+            thumb: 'https://img14.360buyimg.com/mobilecms/s270x270_jfs/t19342/199/1516671468/71132/8e37293d/5acc524cN424bbaa0.jpg!q70.dpg.webp',
+            price: 4599.00,
+            isDouble11: true
+        }]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+
+})
